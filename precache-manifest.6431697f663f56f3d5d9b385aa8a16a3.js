0a48e3823c4e274d3c47f76d42a574a7d2ff4160
Cody Chhay <menghengchhay@gmail.com>
2019-08-21 19:51:10 -0700

    Updates

---
+++ precache-manifest.6431697f663f56f3d5d9b385aa8a16a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e2416d905daf6e80eeb37b267565dd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4d8b33c9a3216009e4bc",
+    "url": "/monsters-rolodex/static/css/main.4592eaa8.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4d8b33c9a3216009e4bc",
+    "url": "/monsters-rolodex/static/js/main.038ab8ad.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
